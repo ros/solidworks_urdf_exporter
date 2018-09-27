f841340347e3b4cdd67be6826778b2b749f473b3
Stephen Brawner <brawner@gmail.com>
2018-09-26 19:50:04 -0700

    Now this works as a pre-build event

--- scripts/UpdateVersionInfo.ps1
+++ scripts/UpdateVersionInfo.ps1
@@ -1,6 +1,9 @@
+param (
+    [string]$filename = "..\..\VersionInfo.cs"
+ )
 
 $CommitVersion = git describe --tags --long --dirty --always
 $FileContent = 'using System.Reflection;
 
 [assembly: AssemblyInformationalVersion("{0}")]' -f $CommitVersion
-$FileContent | Out-File SW2URDF\VersionInfo.cs
+$FileContent | Out-File $filename
