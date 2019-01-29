c3cfb9f158c279292cd5ef7cfa8e66f4302bca9a
Stephen Brawner <brawner@gmail.com>
2019-01-29 15:53:51 -0800

    Adding MessageBox Interface for mocking

---
+++ SW2URDF/UI/IMessageBox.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace SW2URDF.UI
+{
+    public interface IMessageBox
+    {
+        MessageBoxResult Show(string message);
+        MessageBoxResult Show(string message, string caption, MessageBoxButton buttons);
+    }
+}
