11bd2b8a2e68618cf57201ef3ad15f8f61d07045
Stephen Brawner <brawner@gmail.com>
2019-08-01 16:33:29 -0700

    Fix test errors and warnings

--- SW2URDF/UI/MessageBoxHelper.cs
+++ SW2URDF/UI/MessageBoxHelper.cs
@@ -5,12 +5,12 @@ namespace SW2URDF.UI
 {
     public class MessageBoxHelper : IMessageBox
     {
-        MessageBoxResult IMessageBox.Show(string message)
+        public MessageBoxResult Show(string message)
         {
             return MessageBox.Show(message);
         }
 
-        MessageBoxResult IMessageBox.Show(string message, string caption, MessageBoxButton buttons)
+        public MessageBoxResult Show(string message, string caption, MessageBoxButton buttons)
         {
             return MessageBox.Show(message, caption, buttons);
         }
